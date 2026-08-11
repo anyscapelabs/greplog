7775467d5a923b5be1e43231725aa8a6aca2a392
BYRON-lang <byronkennedy22@gmail.com>
2026-08-11 11:35:19 +0200

    dashboard: wordmark back to h-6

--- dashboard/src/components/Header.tsx
+++ dashboard/src/components/Header.tsx
@@ -3,7 +3,7 @@ import wordmark from '../assets/wordmark-white.svg'
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-zinc-800 px-3 py-2">
-      <img src={wordmark} alt="Greplog" className="h-5 w-auto" />
+      <img src={wordmark} alt="Greplog" className="h-6 w-auto" />
     </header>
   )
 }
